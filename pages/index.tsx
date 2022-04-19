b6acf9d85bce65d0c7f25590c999b9dd7889f346
maxtsh <tatshahdoost@gmail.com>
2022-04-19 21:25:06 +0430

    third commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,24 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
         <title>MaxTsh Music App</title>
-        <meta name="description" content="MaxTsh Music App will make you enjoy your time!" />
+        <meta
+          name="description"
+          content="MaxTsh Music App will make you enjoy your time!"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
         <h1>Welcome to Max Tsh Music App</h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
